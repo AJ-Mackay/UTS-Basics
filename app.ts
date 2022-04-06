4203230472ace0ac69fe2e868b6bf9320f9658ca
Ash Mackay <ash.mackay85@gmail.com>
2022-04-06 13:49:15 +0100

    Working with Tuples

--- app.ts
+++ app.ts
@@ -2,11 +2,20 @@
 //   name: string;
 //   age: number;
 // } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Ash",
   age: 37,
   hobbies: ["Reading", "Puzzles"],
+  role: [7, "reader"],
 };
+
+person.role.push("user");
+// person.role[1] = 10;
 
 let favouriteActivities: string[];
 favouriteActivities = ["Reading"];
@@ -15,5 +24,5 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
-//   console.log(hobby.map()); // !!! ERROR !!!
+  //   console.log(hobby.map()); // !!! ERROR !!!
 }
