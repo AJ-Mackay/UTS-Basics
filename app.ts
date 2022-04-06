6f402651f55e9392f02fe958be9b0f74542233cb
Ash Mackay <ash.mackay85@gmail.com>
2022-04-06 13:42:32 +0100

    Array Types

--- app.ts
+++ app.ts
@@ -5,6 +5,15 @@
 const person = {
   name: "Ash",
   age: 37,
+  hobbies: ["Reading", "Puzzles"],
 };
 
+let favouriteActivities: string[];
+favouriteActivities = ["Reading"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+//   console.log(hobby.map()); // !!! ERROR !!!
+}
